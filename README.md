26c6dd603731d364240fba820b468175905e02b0
Taha <taha@meristemcreative.com>
2024-01-28 21:24:34 +0000

    Update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# PROJECT-CST3145-CW2 Express Backend
+
+# PROJECT-CST3145-CW2 AWS Express Backend 
 This Vue.js web application, developed by Taha Farooqui and hosted on Elastic Beanstalk, utilizes a MongoDB database with two collections: Lessons and Orders. It is designed as a mobile app to manage lessons and a shopping cart, offering various functionalities to enhance the user experience.
 
 ## Frontend - Vue.js Application
@@ -19,3 +20,76 @@ This Vue.js web application, developed by Taha Farooqui and hosted on Elastic Be
 For more detailed information about each component, please refer to their respective repositories. Each repository includes specific instructions on setup, usage, and deployment.
 
 Thank you for visiting my project!
+
+## Mdxapp-cw2 Postman Collection
+
+This document describes the Postman collection for the Mdxapp-cw2 API.
+
+
+### Endpoints
+
+#### Lessons
+
+##### All Lessons
+
+- **Method:** GET
+- **URL:** `{{base_url}}/lessons/all-lessons?sort=LessonName&order=asc&search`
+- **Test Script:** 
+  ```javascript
+  pm.test("Status test", function () {
+    pm.response.to.have.status(200);
+    var json = pm.response.json();
+    pm.expect(json.msg).to.equal("success");
+  });
+  ```
+
+##### Update Lessons
+
+- **Method:** PUT
+- **URL:** `{{base_url}}/lessons/update-lessons`
+- **Request Body:**
+  ```json
+  [
+    {"lessonId":"65b5cafa3fcd2c3adc0ed67f","newSpace":3}
+  ]
+  ```
+
+#### Orders
+
+##### Get Order by ID
+
+- **Method:** GET
+- **URL:** `{{base_url}}/orders/get-order-by-id/65b6c117f3d1851be69dcb6e`
+
+##### Create Order
+
+- **Method:** POST
+- **URL:** `{{base_url}}/orders/create-order`
+- **Request Body:**
+  ```json
+  {
+    "username":"Taha Farooqui",
+    "contact":"746564536",
+    "items":[
+        {
+            "_id":"65b5cafa3fcd2c3adc0ed681",
+            "LessonName":"Art History",
+            "LessonLocation":"London",
+            "LessonPrice":109,
+            "TotalLessonSpace":9,
+            "LessonSpace":1
+        }
+    ],
+    "totalAmount":109
+  }
+  ```
+
+##### Delete Order
+
+- **Method:** DELETE
+- **URL:** `{{base_url}}/orders/delete-order`
+
+##### All Orders
+
+- **Method:** GET
+- **URL:** `{{base_url}}/orders/all-orders`
--- routes/orders.js
+++ routes/orders.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ObjectId } = require('mongodb');
 
 // Function to setup routes for 'orders'
 function ordersRoutes(client) {
@@ -11,16 +12,7 @@ function ordersRoutes(client) {
         next();
     }
 
-    // Middleware for validating new order data
-    function validateNewOrder(req, res, next) {
-        const newOrder = req.body;
-        // Example validation: Ensure that newOrder has a title and status
-        if (!newOrder.title || !newOrder.status) {
-            return res.status(400).send("Invalid order data");
-        }
-        next();
-    }
-
+    
     // GET route to fetch all orders with optional filters
     router.get('/all-orders', logRequest, async (req, res) => {
         try {
@@ -42,7 +34,7 @@ function ordersRoutes(client) {
     // GET route to fetch a specific order by ID
     router.get('/get-order-by-id/:id', logRequest, async (req, res) => {
         try {
-            const order = await collection.findOne({ _id: new client.ObjectID(req.params.id) });
+            const order = await collection.findOne({ _id: new ObjectId(req.params.id) });
             if (!order) return res.status(404).send('Order not found');
             res.json(order);
         } catch (e) {
@@ -51,11 +43,11 @@ function ordersRoutes(client) {
     });
 
     // POST route to create a new order
-    router.post('/create-order', [logRequest, validateNewOrder], async (req, res) => {
+    router.post('/create-order', logRequest, async (req, res) => {
         try {
             const newOrder = req.body;
             const result = await collection.insertOne(newOrder);
-            res.status(201).json(result.ops);
+            res.status(201).json(result);
         } catch (e) {
             console.error(e);
             res.status(500).send(e.message);
@@ -65,7 +57,7 @@ function ordersRoutes(client) {
     // DELETE route to delete an order by ID
     router.delete('/delete-order/:id', logRequest, async (req, res) => {
         try {
-            const result = await collection.deleteOne({ _id: new client.ObjectID(req.params.id) });
+            const result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
             if (result.deletedCount === 0) return res.status(404).send('Order not found');
             res.status(204).send();
         } catch (e) {
